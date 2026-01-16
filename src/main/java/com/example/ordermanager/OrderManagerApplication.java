0bc48a473e02f1e784e7bf4dbb2ef61a067e7983
liwy <535767595@qq.com>
2026-01-16 14:53:58 +0800

    feat((src)): 使用java21

--- src/main/java/com/example/ordermanager/OrderManagerApplication.java
+++ src/main/java/com/example/ordermanager/OrderManagerApplication.java
@@ -9,6 +9,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 public class OrderManagerApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Starting Order Manager Application...");
 		SpringApplication.run(OrderManagerApplication.class, args);
 	}
 
